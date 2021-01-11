8f9406c6ccd8593201fe49f3c67e47f23c10c00c
Martín Melo <martin.melo.dev.97@gmail.com>
2021-01-11 10:07:32 -0600

    Refactored myCodeChallenge.kt

--- module-5/myCodeChallege.kt
+++ module-5/myCodeChallege.kt
@@ -5,14 +5,17 @@
 // 4. Using when get the month's name of the value,
 // 5. If the value is bigger than 12, show "Not month available"
 
-package conditionals
-
 import java.util.*
 
 
 fun main() {
     val num1 = Random().nextInt(10)
     val num2 = Random().nextInt(10)
+    val months = listOf(
+        "January", "February", "March", "April",
+        "May", "June", "July", "August", "September",
+        "October", "November", "December"
+    )
     println("Num1: $num1")
     println("Num2: $num2")
     if (num1 <= 0 || num2 <= 0) {
@@ -20,18 +23,7 @@ fun main() {
     } else {
         val sum = num1 + num2
         val monthName = when (sum) {
-            1 -> "January"
-            2 -> "February"
-            3 -> "March"
-            4 -> "April"
-            5 -> "May"
-            6 -> "June"
-            7 -> "July"
-            8 -> "August"
-            9 -> "September"
-            10 -> "October"
-            11 -> "November"
-            12 -> "Dicember"
+            in 1..13 -> months[sum - 1]
             else -> "Not month available"
         }
         println("Month name: $monthName")
